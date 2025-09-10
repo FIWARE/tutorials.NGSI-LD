0dd0bdccfaef99f8805c139e398fde3f7ab888c9
Jason Fox <jason-fox@users.noreply.github.com>
2025-09-10 23:05:33 +0200

    Display added data

---
+++ app/controllers/ngsi-ld/pest.js
@@ -0,0 +1,38 @@
+const debug = require('debug')('tutorial:pest');
+const monitor = require('../../lib/monitoring');
+const ngsiLD = require('../../lib/ngsi-ld');
+const Context = process.env.IOTA_JSON_LD_CONTEXT || 'http://context/ngsi-context.jsonld';
+const LinkHeader = '<' + Context + '>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json">';
+
+async function displayPest(req, res) {
+    debug('displayPest');
+    // If the user is not authorized, display the main page.
+    if (!res.locals.authorized) {
+        req.flash('error', 'Access Denied');
+        return res.redirect('/');
+    }
+    try {
+        monitor('NGSI', 'readEntity ' + req.params.id);
+        const pest = await ngsiLD.readEntity(
+            req.params.id,
+            { options: 'keyValues' },
+            ngsiLD.setHeaders(req.session.access_token, LinkHeader)
+        );
+        return res.render('pest', { title: pest.name, pest });
+    } catch (error) {
+        const errorDetail = error.error;
+        debug(errorDetail);
+        // If no animal has been found, display an error screen
+        return res.render('error', {
+            title: `Error: ${errorDetail.title}`,
+            message: errorDetail.detail,
+            error: {
+                stack: errorDetail.title
+            }
+        });
+    }
+}
+
+module.exports = {
+    display: displayPest
+};
---
+++ app/controllers/ngsi-ld/weather.js
@@ -0,0 +1,38 @@
+const debug = require('debug')('tutorial:weather');
+const monitor = require('../../lib/monitoring');
+const ngsiLD = require('../../lib/ngsi-ld');
+const Context = process.env.IOTA_JSON_LD_CONTEXT || 'http://context/ngsi-context.jsonld';
+const LinkHeader = '<' + Context + '>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json">';
+
+async function displayWeather(req, res) {
+    debug('displayWeather');
+    // If the user is not authorized, display the main page.
+    if (!res.locals.authorized) {
+        req.flash('error', 'Access Denied');
+        return res.redirect('/');
+    }
+    try {
+        monitor('NGSI', 'readEntity ' + req.params.id);
+        const weather = await ngsiLD.readEntity(
+            req.params.id,
+            { options: 'keyValues' },
+            ngsiLD.setHeaders(req.session.access_token, LinkHeader)
+        );
+        return res.render('weather', { title: weather.name, weather });
+    } catch (error) {
+        const errorDetail = error.error;
+        debug(errorDetail);
+        // If no animal has been found, display an error screen
+        return res.render('error', {
+            title: `Error: ${errorDetail.title}`,
+            message: errorDetail.detail,
+            error: {
+                stack: errorDetail.title
+            }
+        });
+    }
+}
+
+module.exports = {
+    display: displayWeather
+};
