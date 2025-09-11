6d2636527da418837f6f1c82cad8ea9f4da79d1b
Jason Fox <jason-fox@users.noreply.github.com>
2025-09-11 11:21:31 +0200

    Switch to maplibre

--- app/controllers/ngsi-ld/farm.js
+++ app/controllers/ngsi-ld/farm.js
@@ -10,26 +10,6 @@ const ngsiLD = require('../../lib/ngsi-ld');
 const Context = process.env.IOTA_JSON_LD_CONTEXT || 'http://context/ngsi-context.jsonld';
 const LinkHeader = '<' + Context + '>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json">';
 
-function mapTileUrl(zoom, location) {
-    const tilesPerRow = Math.pow(2, zoom);
-    let longitude = location.coordinates[0];
-    let latitude = location.coordinates[1];
-
-    longitude /= 360;
-    longitude += 0.5;
-    latitude = 0.5 - Math.log(Math.tan(Math.PI / 4 + (latitude * Math.PI) / 360)) / Math.PI / 2.0;
-
-    return (
-        'https://a.tile.openstreetmap.org/' +
-        zoom +
-        '/' +
-        Math.floor(longitude * tilesPerRow) +
-        '/' +
-        Math.floor(latitude * tilesPerRow) +
-        '.png'
-    );
-}
-
 // This function receives the details of a building from the context
 //
 // It is effectively processing the following cUrl command:
@@ -50,8 +30,6 @@ async function displayFarm(req, res) {
             { options: 'keyValues' },
             ngsiLD.setHeaders(req.session.access_token, LinkHeader)
         );
-        // If a building has been found display it on screen
-        building.mapUrl = mapTileUrl(15, building.location);
         return res.render('building', { title: building.name, building });
     } catch (error) {
         // If no farm has been found, display an error screen
