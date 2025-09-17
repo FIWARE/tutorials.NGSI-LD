06de9d59a18bd3e1b7ac80618c3bef6590d3d841
Jason Fox <jason-fox@users.noreply.github.com>
2025-09-17 20:27:06 +0200

    More internationalization.

--- app/controllers/ngsi-ld/farm.js
+++ app/controllers/ngsi-ld/farm.js
@@ -8,7 +8,11 @@ const debug = require('debug')('tutorial:farm');
 const monitor = require('../../lib/monitoring');
 const ngsiLD = require('../../lib/ngsi-ld');
 const Context = process.env.IOTA_JSON_LD_CONTEXT || 'http://context/ngsi-context.jsonld';
+const Context_EN = Context.replace('.jsonld', '-en.jsonld')
+const Context_FR = Context.replace('.jsonld', '-fr.jsonld')
 const LinkHeader = '<' + Context + '>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json">';
+const LinkHeader_EN = '<' + Context_EN + '>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json">';
+const LinkHeader_FR = '<' + Context_FR + '>; rel="http://www.w3.org/ns/json-ld#context"; type="application/ld+json">';
 
 // This function receives the details of a building from the context
 //
@@ -20,14 +24,27 @@ async function displayFarm(req, res) {
     debug('displayFarm');
     // If the user is not authorized, display the main page.
 
+    let UseLinkHeader = LinkHeader;
+
+    if (req.query.lang === 'en'){
+        UseLinkHeader = LinkHeader_EN;
+    }
+    if (req.query.lang === 'fr'){
+        UseLinkHeader = LinkHeader_FR;
+    }
+    if (req.query.lang === 'none'){
+        UseLinkHeader = null;
+    }
+
     try {
         monitor('NGSI', 'readEntity ' + req.params.id);
         const building = await ngsiLD.readEntity(
             req.params.id,
             { options: 'normalized', local: req.query.local },
-            ngsiLD.setHeaders(null, LinkHeader)
+            ngsiLD.setHeaders(null, UseLinkHeader)
         );
-        return res.render('building', { title: building.name.value, building });
+        const title = building.name ? building.name.value : building.nom.value;
+        return res.render('building', { title, building });
     } catch (error) {
         // If no farm has been found, display an error screen
         console.log(error);
